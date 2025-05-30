2039ffdb72d371b7e8e4f025684b96d42198504e
kyleliu78 <liuk8504@lgsstudent.org>
2025-05-30 13:35:08 -0700

    j

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
-let score = 5;
+let score = 0;
 const scoreDisplay = document.getElementById("score");
 const button = document.getElementById("clickBtn");
+<<<<<<< HEAD
 const button2 = document.getElementById("clickBtn");
     const canvas = document.getElementById('myCanvas');
     const ctx = canvas.getContext('2d');
     // Example: Draw a red rectangle
     ctx.fillStyle = 'red';
     ctx.fillRect(50, 50, 100, 75);
+=======
+>>>>>>> parent of f3cbc4d (experimentation)
 
-    // Example: Draw a blue circle
-    ctx.beginPath();
-    ctx.arc(150, 100, 40, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.stroke();
-
-    // Example: Draw a green triangle
-    ctx.beginPath();
-    ctx.moveTo(200, 150);
-    ctx.lineTo(250, 75);
-    ctx.lineTo(300, 150);
-    ctx.closePath();
-    ctx.strokeStyle = 'green';
-    ctx.stroke();
-
-
-    function showGif() {
-  const container = document.getElementById("gifContainer");
-  const img = document.createElement("img");
-  img.src = "funny.gif"; // make sure this file is in your project folder
-  img.alt = "Funny gif";
-  img.width = 300; // optional
-  container.appendChild(img);
-}
 button.addEventListener("click", () => {
   score++;
   scoreDisplay.textContent = score;
 });
+<<<<<<< HEAD
 
 function changeScreen() {
 button2.addEventListener("click", () => {
@@ -44,3 +23,5 @@ button2.addEventListener("click", () => {
   scoreDisplay.textContent = score;
 });
 
+=======
+>>>>>>> parent of f3cbc4d (experimentation)

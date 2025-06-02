24d7906ddf7edea040f06338e7708c9ced8077ed
kyleliu78 <liuk8504@lgsstudent.org>
2025-06-01 23:47:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,62 +3,65 @@ const catInput = document.querySelector(".catInput");
 const p = document.querySelector(".display");
 const api = "live_lQXHOtO3Bbtn1IaotlU2dn2YjNylGcccziOWNj8383hANHWeZjQGUAqKTPMG8bq2";
 
-catForm.addEventListener("submit", async event=>{
+catForm.addEventListener("submit", async event => {
     event.preventDefault();
     const cat = catInput.value.trim().toLowerCase();
-    if (cat){
-        try{
+    if (cat) {
+        try {
             const catInfo = await getCatInfo(cat);
+            if (!catInfo) {
+                throw new Error("Breed not found");
+            }
             const imageUrl = await getImage(catInfo.id);
-            p.innerHTML = 
-            <img src="${imageUrl}"style="max-width: 300px;">
-            <p>${catInfo.description}</p>
-            ;
-        }
-        catch(error){
+            p.innerHTML = `
+                <img src="${imageUrl}" style="max-width: 300px;">
+                <p>${catInfo.description}</p>
+            `;
+        } catch (error) {
             console.error(error);
             displayError("Not a valid breed");
         }
-    }
-    else{
+    } else {
         displayError("Please enter a cat breed");
     }
-})
+});
 
-async function getCatInfo(breed){
+async function getCatInfo(breed) {
     const response = await fetch("https://api.thecatapi.com/v1/breeds", {
         headers: {
             "x-api-key": api
         }
     });
-    console.log(response);
-    if (!response.ok){
-        throw new Error ("error fetching description");
+
+    if (!response.ok) {
+        throw new Error("Error fetching description");
     }
-    const j =  await response.json();
-    return j.find(b => b.name.toLowerCase() === breed);
+
+    const data = await response.json();
+    return data.find(b => b.name.toLowerCase() === breed);
 }
 
-async function getImage(breed){
-    const response = await fetch(https://api.thecatapi.com/v1/images/search?breed_ids=${breed}, {
+async function getImage(breedId) {
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
         headers: {
             "x-api-key": api
         }
     });
-    console.log(response);
-    if (!response.ok){
-        throw new Error ("error fetching image");
+
+    if (!response.ok) {
+        throw new Error("Error fetching image");
     }
-    const j = await response.json();
-    return j[0]?.url;
+
+    const data = await response.json();
+    return data[0]?.url;
 }
 
-function displayError(message){
+function displayError(message) {
     const error = document.createElement("p");
     error.textContent = message;
     error.classList.add("errorDisplay");
 
-    p.textContent="";
-    p.style.display="flex";
+    p.textContent = "";
+    p.style.display = "flex";
     p.appendChild(error);
 }
